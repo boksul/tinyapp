1feb7d528bfded5e5c87cd3120dc49fb95ba1996
boksul <boksul@hotmail.com>
2019-02-14 20:31:11 +0000

    express_server.js added

--- express_server.js
+++ express_server.js
@@ -4,23 +4,25 @@ var PORT = 8080;
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
-
 function generateRandomString() {
-  let shortURL = "";
+  let unique = "";
   let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   for (let i = 0; i < 6; i++) {
-    shortURL += possible.charAt(Math.floor(Math.random() * possible.length));
+    unique += possible.charAt(Math.floor(Math.random() * possible.length));
   }
-  return shortURL
-}
-console.log(generateRandomString());
+  return unique;
+};
+
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -30,9 +32,12 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send(shortURL);         // Respond with 'Ok' (we will replace this)
+app.post("/urls", (req, res) => {  // Log the POST request body to the console
+  const newShortURL = generateRandomString()
+  urlDatabase[generateRandomString()] = req.body.longURL
+  console.log("urlDatabase", urlDatabase)
+  // res.send("ok");         // Respond with 'Ok' (we will replace this)
+  res.redirect('/urls/'+newShortURL);
 });
 
 app.get("/urls.json", (req, res) => {
@@ -55,4 +60,16 @@ app.get("/urls", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  if(urlDatabase[req.params.shortURL]) {
+    let shortURL = req.params.shortURL;
+    let longURL = urlDatabase[shortURL].url;
+    urlDatabase[shortURL].counter++;
+    res.redirect(longURL);
+  } else {
+    res.sendStatus(400);
+  }
+  // const longURL = ...
 });
